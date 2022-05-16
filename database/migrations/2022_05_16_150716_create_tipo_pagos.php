ca315f10d7de077214b1eab8ee2efe4ca7e3f87b
KabirLicona <102567212+KabirLicona@users.noreply.github.com>
2022-05-16 17:19:52 +0200

    actualizacion de migraciones en bitacora no se como agregar usuarios

--- database/migrations/2022_05_16_150716_create_tipo_pagos.php
+++ database/migrations/2022_05_16_150716_create_tipo_pagos.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class TipoReporte extends Migration
+class CreateTipoPagos extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class TipoReporte extends Migration
      */
     public function up()
     {
-        Schema::create('tipo_reporte', function (Blueprint $table) {
-            $table->id();
-            $table->string('descripcion');
-            $table->boolean('estado');
+        Schema::create('tipo_pagos', function (Blueprint $table) {
+            $table->bigIncrements('COD_TIPO_PAGO');
+            $table->enum("TIPO_CREDITO",["T","CK","E"]);
+            $table->string('DESCRIPCION');
             $table->timestamps();
+            $table->softDeletes(); //ESTE LO AGREGUE PARA QUE SE MIRE LA FECHA DE ELIMINACION
         });
     }
 
@@ -28,5 +29,6 @@ class TipoReporte extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('tipo_pagos');
     }
 }
